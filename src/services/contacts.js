8a87e37e5ecf6525ea52d709ba99fd7c09ef2ad5
Serhii <serhii1992ser@gmail.com>
2025-05-24 13:49:25 +0200

    hw-04

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,39 @@
 import Contact from '../models/contactModel.js';
 
-export const getAllContacts = async () => Contact.find();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortBy = 'name',
+  sortOrder = 'asc',
+  type,
+  isFavourite,
+}) => {
+  const filter = {};
+  if (type) filter.contactType = type;
+  if (typeof isFavourite !== 'undefined') filter.isFavourite = isFavourite;
+
+  const sort = { [sortBy]: sortOrder === 'desc' ? -1 : 1 };
+
+  const skip = (page - 1) * perPage;
+
+  const [data, totalItems] = await Promise.all([
+    Contact.find(filter).sort(sort).skip(skip).limit(perPage),
+    Contact.countDocuments(filter),
+  ]);
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  return {
+    data,
+    page: Number(page),
+    perPage: Number(perPage),
+    totalItems,
+    totalPages,
+    hasPreviousPage: page > 1,
+    hasNextPage: page < totalPages,
+  };
+};
+
 
 export const getContactById = async (id) => Contact.findById(id);
 
