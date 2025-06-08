80b8af32d717e5192030c1950edf984176be864e
Serhii <serhii1992ser@gmail.com>
2025-06-08 11:43:19 +0200

    hw-fix

--- src/schemas/contactSchemas.js
+++ src/schemas/contactSchemas.js
@@ -5,6 +5,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().optional(),
   isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
@@ -12,4 +13,8 @@ export const updateContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20).optional(),
   email: Joi.string().email().optional(),
   isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 }).min(1);
+
+
+
