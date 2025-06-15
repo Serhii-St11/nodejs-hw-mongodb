8f2defcb73e17834490dd2528be1e102cb0b7b91
Serhii <serhii1992ser@gmail.com>
2025-06-15 15:58:34 +0200

    hw-07

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
+
 import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+import apiDocsRouter from '../src/routers/api-docs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -24,8 +27,10 @@ export const setupServer = () => {
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
-  app.use(notFoundHandler);
+  // Swagger docs route
+  app.use('/api-docs', apiDocsRouter);
 
+  app.use(notFoundHandler);
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
@@ -33,4 +38,3 @@ export const setupServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
-
